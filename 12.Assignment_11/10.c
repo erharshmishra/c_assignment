d975b183ac8377618818818e572b8cdd18821964
Harsh Mishra <154444345+erharshmishra@users.noreply.github.com>
2026-01-03 12:32:11 +0530

    Add files via upload

---
+++ 12.Assignment_11/10.c
@@ -0,0 +1,13 @@
+// Write a program to print a table of 5.
+
+#include <stdio.h>
+int main()
+{
+    int i = 1;
+    while (i <= 10)
+    {
+        printf(" 5 x %d =  %d\n", i, i * 5);
+        i++;
+    }
+    return 0;
+}
---
+++ 12.Assignment_11/5.c
@@ -0,0 +1,13 @@
+// Write a program to print the first 10 odd natural numbers in reverse order.
+
+#include <stdio.h>
+int main()
+{
+    int i = 1;
+    while (i <= 10)
+    {
+        printf("%d\n", 21 - 2 * i);
+        i++;
+    }
+    return 0;
+}
